b87e140f6fe819d38cdd08d9b0ae6bfca4bc6ace
Jorge Alves <jorge.mnf.alves@gmail.com>
2025-06-10 13:24:03 +0200

    none

--- public/api/index.php
+++ public/api/index.php
@@ -6,6 +6,11 @@
 // Description: Central API router for all incoming public requests.
 // ============================================
 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // [0] Loading settings and variables
 require_once __DIR__ . '/../../init.php';
 require_once BASEPATH . '/backend/utils/helpers.php';
